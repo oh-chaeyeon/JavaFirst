badd333a033dcfe71efe9833cabc98c16c84eb88
오채연 <115947715+oh-chaeyeon@users.noreply.github.com>
2023-07-26 00:05:12 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner oh =new Scanner(System.in);
+        int a = oh.nextInt();
+        int b = oh.nextInt();
+        
+        System.out.println(a+b);
+        oh.close();
+    }
+}
