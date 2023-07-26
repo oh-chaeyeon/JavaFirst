e83e849ad5a91ebdcbb149e12625fe8323d96d0f
오채연 <115947715+oh-chaeyeon@users.noreply.github.com>
2023-07-26 12:15:24 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner oh = new Scanner(System.in);
+        int n1 = oh.nextInt();
+        int n2 = oh.nextInt();
+        
+        System.out.println(n1-n2);
+    }
+}
