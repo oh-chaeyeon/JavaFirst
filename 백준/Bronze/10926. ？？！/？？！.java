3598dc570d563b495780a269317cc86e20d8a512
오채연 <115947715+oh-chaeyeon@users.noreply.github.com>
2023-07-26 12:51:03 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner oh = new Scanner(System.in);
+        String id = oh.next();
+        
+        String num = "??!";
+        System.out.println(id+num);
+        
+    }
+}
