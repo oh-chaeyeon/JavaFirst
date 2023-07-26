780df51140e5b344bff431399f8ee9ccd32129bb
오채연 <115947715+oh-chaeyeon@users.noreply.github.com>
2023-07-26 12:53:55 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner oh = new Scanner(System.in);
+        int year = oh.nextInt();
+        System.out.println(year-543);
+        
+    }
+}
