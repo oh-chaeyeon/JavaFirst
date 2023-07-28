92bb924c2e7793b0a14af53a97dc641c3ca9fe64
오채연 <115947715+oh-chaeyeon@users.noreply.github.com>
2023-07-28 23:18:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner oh = new Scanner(System.in);
+        
+       	 	int n1 = oh.nextInt();
+        	int n2 = oh.nextInt();
+        	int n3 = oh.nextInt();
+        
+        	int result = 0;
+        
+        	if (n1 == n2 && n2 == n3 && n1 == n3) {
+        		result = 10000 + n1 * 1000;
+        	}
+        	else if (n1 == n2 || n1 == n3) {
+        		result = 1000 + n1 * 100;
+        	}
+        	else if (n2 == n3) {			
+        		result = 1000 + n2 * 100;
+        	}
+        	else {
+        		result = Math.max(n1,  Math.max(n2, n3)) * 100;
+        	}
+        
+        	System.out.println(result);
+        	oh.close();
+	}
+}
