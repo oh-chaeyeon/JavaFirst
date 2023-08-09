199437b673b3a7382169144200f982ce04c1173b
oh-chaeyeon <115947715+oh-chaeyeon@users.noreply.github.com>
2023-08-09 23:18:08 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 392 ms, Memory: 19092 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        // 첫 번째 행렬 입력 받기
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 두 번째 행렬 입력 받기
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 두 행렬을 더한 결과 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
