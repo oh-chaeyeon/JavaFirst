bb15dc6feef32f821c23f74e994b93c0e4db9816
oh-chaeyeon <115947715+oh-chaeyeon@users.noreply.github.com>
2023-08-23 23:19:52 +0900

    [Bronze I] Title: 최소공배수, Time: 192 ms, Memory: 14964 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); 
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int gcd = findGCD(A, B);
+            int lcm = A * B / gcd;
+
+            System.out.println(lcm);
+        }
+        br.close();
+    }
+
+    // 최대공약수를 구하는 함수
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
