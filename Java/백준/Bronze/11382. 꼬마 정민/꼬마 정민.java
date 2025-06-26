a09a2bfd44385cb41a9798bcfa50e9764b5f42d0
oh-chaeyeon <115947715+oh-chaeyeon@users.noreply.github.com>
2025-06-26 22:41:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 228 ms, Memory: 17760 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        
+	Scanner oh = new Scanner(System.in);
+    long a = oh.nextLong();
+    long b = oh.nextLong();
+    long c = oh.nextLong();
+        
+    System.out.println(a+b+c);
+    oh.close();
+    }
+}
